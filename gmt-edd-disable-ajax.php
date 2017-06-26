75304dde8ecded2fb9e06cccd35fd5378736b77e
cferdinandi <chris@gomakethings.com>
2017-06-26 10:15:06 -0400

    v1.2.0 Allow AJAX on checkout

--- gmt-edd-disable-ajax.php
+++ gmt-edd-disable-ajax.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/cferdinandi/gmt-edd-disable-ajax/
  * GitHub Plugin URI: https://github.com/cferdinandi/gmt-edd-disable-ajax/
  * Description: Reenables the "Disable AJAX Checkout" functionality in Easy Digital Downloads.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Chris Ferdinandi
  * Author URI: http://gomakethings.com
  * License: GPLv3
@@ -13,6 +13,7 @@
 
 
 function gmt_edd_disable_ajax() {
+	if ( function_exists( 'edd_is_checkout' ) && edd_is_checkout() ) return false;
 	return true;
 }
 add_filter( 'edd_is_ajax_disabled', 'gmt_edd_disable_ajax' );
